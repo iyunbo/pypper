1b0ba674fb240790ac4480112084672acebd2203
iyunbo <yunbo.wang@solven.eu>
2020-08-08 14:12:38 +0200

    fix license issue

--- setup.py
+++ setup.py
@@ -7,16 +7,15 @@ from setuptools import setup, find_packages
 with open("README.md") as f:
     readme = f.read()
 
-with open("LICENSE") as f:
-    project_license = f.read()
-
 setup(
     name="pypper",
-    version="0.1.1",
+    version="0.1.2",
     description="A developer toolbox not only for Python",
     author="Yunbo WANG",
     author_email="w.yunbo@gmail.com",
     url="https://github.com/iyunbo/pypper",
-    license=project_license,
+    license="MIT License",
+    long_description_content_type="text/markdown",
+    long_description=readme,
     packages=find_packages(exclude=("tests", "docs"))
 )
